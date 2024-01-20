1972a7a647b21249cbecd6dad4aaf56fc120cd80
Daniel Ceballos <danielceballos94@gmail.com>
2024-01-19 19:27:09 -0500

    question 1 done

--- question1/main.ts
+++ question1/main.ts
@@ -1,4 +1,28 @@
 export function isValidWalk(walk: string[]) {
-  
-}
+  //early return if we don't have 10 steps
+  if (walk.length !== 10) {
+    console.log("Walk does not have 10 steps");
+    return false;
+  }
 
+  //set n,s,e,w as coordinates
+  let x = 0
+  let y = 0
+
+  //if n or s, y+ or y-; if e or w x+ or x-
+  //loop walk string
+  walk.forEach(step => {
+    if(step === 'n'){
+        y++
+    } else if (step === 's'){
+        y--
+    } else if (step === 'e'){
+        x++
+    }else if (step === 'w'){
+        x--
+    }
+  })
+
+  //if x and y are 0, we made it to the start
+  return (x === 0 && y === 0)
+}
