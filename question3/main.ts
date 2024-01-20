8e0e4a03e7c1713373c5a8fb2b963ba56fda4b58
Daniel Ceballos <danielceballos94@gmail.com>
2024-01-19 20:33:35 -0500

    add column full condition

--- question3/main.ts
+++ question3/main.ts
@@ -5,8 +5,8 @@ class Connect4 {
 
   constructor() {
     // Good luck
-    //create the array of 6 columns and 7 rows on each
-    this.grid = Array.from({ length: 6 }, () => Array(7).fill(0));
+    //create the array of 7 columns and 6 rows on each
+    this.grid = Array.from({ length: 7 }, () => Array(6).fill(0));
     this.currentPlayer = 1
   }
 
@@ -14,6 +14,22 @@ class Connect4 {
     // Good luck
     console.log("disc place in column ", col);
     console.log(`Player ${this.currentPlayer} has a turn`)
+    //check if column is full
+    if(this.grid[col].filter(row => row === 0).length === 0){
+      console.log(`Column ${col} is full`)
+      return "Column full!"
+    }
+
+    //loop starting from the last row in the given column, after finding a 0, change it to 1
+    for(let row = this.grid[col].length-1; row >= 0; row--){
+      if(this.grid[col][row] === 0){
+
+        this.grid[col][row] = 1
+
+        break
+      }
+    }
+    // console.log('grid is here ', this.grid[3])
     this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
     return `Player has a turn`;
   }
@@ -21,7 +37,8 @@ class Connect4 {
 
 const newGame = new Connect4();
 newGame.play(3)
-newGame.play(2)
+newGame.play(3)
+newGame.play(3)
 newGame.play(3)
 newGame.play(3)
 newGame.play(3)
