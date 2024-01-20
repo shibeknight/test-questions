74af7faf8427b1c6d378fe5e136649d2aa976a79
Daniel Ceballos <danielceballos94@gmail.com>
2024-01-19 19:43:11 -0500

    question 2 complete

--- question2/main.ts
+++ question2/main.ts
@@ -1,3 +1,27 @@
 export function findOutlier(integers: number[]): number {
-    
+
+    //count even or odd numbers, check which "type of array" we got
+    let evenNumbers = 0
+    let oddNumbers = 0
+    //store last of each type
+    let even = 0;
+    let odd = 0;
+    //loop through the numbers
+    integers.forEach(integer => {
+      //if even, increase count and save
+      if(integer % 2 === 0) {
+        evenNumbers++
+        even = integer
+      //same for odd
+      } else {
+        oddNumbers++
+        odd = integer
+      }
+    })
+    //if only 1 even number, that means all others are odd and so we return the last/only even number stored
+    if(evenNumbers === 1){
+      return even
+    }
+    //if that is not true, we simply return the odd number
+    return odd
   }
