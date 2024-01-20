86c4dcf55f0acf75c0ee6fffec1005c4111b250c
Daniel Ceballos <danielceballos94@gmail.com>
2024-01-19 20:07:25 -0500

    basic turn, column select, alternate players

--- question3/main.ts
+++ question3/main.ts
@@ -1,10 +1,28 @@
-export class Connect4 {
+class Connect4 {
+  //create a multidimensional array to handle the grid(columns and rows) of the board
+  private grid: number[][]
+  private currentPlayer: number
 
-    constructor() {
-      // Good luck
-    }
-  
-    play(col: number): string{
-      // Good luck
-    }
+  constructor() {
+    // Good luck
+    //create the array of 6 columns and 7 rows on each
+    this.grid = Array.from({ length: 6 }, () => Array(7).fill(0));
+    this.currentPlayer = 1
   }
+
+  play(col: number): string {
+    // Good luck
+    console.log("disc place in column ", col);
+    console.log(`Player ${this.currentPlayer} has a turn`)
+    this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+    return `Player has a turn`;
+  }
+}
+
+const newGame = new Connect4();
+newGame.play(3)
+newGame.play(2)
+newGame.play(3)
+newGame.play(3)
+newGame.play(3)
+newGame.play(3)
